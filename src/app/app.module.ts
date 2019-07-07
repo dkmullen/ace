e37bf97155e3ec00582fb7889f4e16751f736a0e
Dennis <dkmullen@gmail.com>
2019-07-06 20:36:51 -0400

    menu and board

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MenuComponent } from './menu/menu.component';
+import { BrandingComponent } from './branding/branding.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BoardComponent } from './pages/about/board/board.component';
+import { MissionComponent } from './pages/about/mission/mission.component';
+import { SponsorsComponent } from './pages/about/sponsors/sponsors.component';
+import { SupportComponent } from './pages/about/support/support.component';
+
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about/mission', component: MissionComponent },
+  { path: 'about/sponsors', component: SponsorsComponent },
+  { path: 'about/board', component: BoardComponent },
+  { path: 'about/support', component: SupportComponent },
+];
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    BrandingComponent,
+    HomeComponent,
+    BoardComponent,
+    MissionComponent,
+    SponsorsComponent,
+    SupportComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+  public wide = true;
+
+  constructor() { }
+
+  ngOnInit() {
+    window.innerWidth > 799 ? this.wide = true : this.wide = false;
+    this.getWidth();
+  }
+
+  getWidth() {
+    window.onresize = () => {
+      window.innerWidth > 799 ? this.wide = true : this.wide = false;
+    }
+  }
+}
