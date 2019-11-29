8b2c7983a773bf0255c9f472c42ec6b5ae0ee3f9
Dennis <dkmullen@gmail.com>
2019-11-29 14:50:26 -0500

    Sponsor bar

--- src/app/pages/about/sponsors/sponsors.component.ts
+++ src/app/pages/about/sponsors/sponsors.component.ts
@@ -1,22 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sponsors',
   templateUrl: './sponsors.component.html',
   styleUrls: ['./sponsors.component.scss']
 })
-export class SponsorsComponent implements OnInit {
-  public sponsorLogos = [
-    '../../../assets/images/sponsors/nationalrx.png',
-    '../../../assets/images/sponsors/sb.png',
-    '../../../assets/images/sponsors/sr.jpg',
-    '../../../assets/images/sponsors/tc.png',
-    '../../../assets/images/sponsors/volunteer-pharmacy.png',
+export class SponsorsComponent {
+  public sponsors = [
+    {
+      logo: '../../../assets/images/sponsors/nationalrx.png',
+      link: 'https://www.nationalrx.com/'
+    },
+    {
+      logo: '../../../assets/images/sponsors/sb.png',
+      link: 'http://springboardav.com/'
+    },
+    {
+      logo: '../../../assets/images/sponsors/sr.jpg',
+      link: 'https://www.schoolofrock.com/'
+    },
+    {
+      logo: '../../../assets/images/sponsors/tc.png',
+      link: 'http://turkeycreek.com/'
+    },
+    {
+      logo: '../../../assets/images/sponsors/volunteer-pharmacy.png',
+      link: 'https://volunteerpharmacy.com/'
+    }
   ];
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
