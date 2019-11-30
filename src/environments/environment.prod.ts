53053c536cda720b6f27273e0ca2e84c7740653b
Dennis <dkmullen@gmail.com>
2019-11-30 11:01:23 -0500

    clear cache

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  actingUrl: 'https://ace-dkm.herokuapp.com/api/acting-posts',
-  singingUrl: 'https://ace-dkm.herokuapp.com/api/posts',
-  actingTeamUrl: 'https://ace-dkm.herokuapp.com/api/acting-team-posts'
-};
