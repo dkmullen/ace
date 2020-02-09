8005da6c2bd67afd6b83daa99546bced156872da
Dennis Mullen <dkmullen@gmail.com>
2020-02-09 12:23:46 -0500

    Refine acting list component

--- src/app/pages/events/acting/acting.component.ts
+++ src/app/pages/events/acting/acting.component.ts
@@ -1,34 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-acting',
   templateUrl: './acting.component.html',
   styleUrls: ['./acting.component.scss']
 })
-export class ActingComponent implements OnInit {
-  comedicStudents = [
-    { name: 'Grace Ballard', school: 'Maryville High School', category: 'comedic', notes: 'winner' },
-    { name: 'Kate Bishop', school: 'Grace Christian Academy', category: 'comedic', notes: 'distinction' },
-    { name: 'Kyla Jernigan', school: 'Homeschool', category: 'comedic', notes: 'distinction' },
-  ];
-  dramaticStudents = [
-    {name: 'LoRen Seagrave', school: 'Austin East', category: 'dramatic', notes: 'winner'},
-    {name: 'Chloe Adsit', school: 'Heritage', category: 'dramatic', notes: 'distinction'},
-    {name: 'Jackson Ahern', school: 'Austin East Magnet', category: 'dramatic', notes: 'distinction'},
-    {name: 'Heather Bohan', school: 'Farragut', category: 'dramatic', notes: 'distinction'},
-    {name: 'Jack Lloyd', school: 'Oak Ridge', category: 'dramatic', notes: 'distinction'},
-    {name: 'Ella Trisler', school: 'Catholic', category: 'dramatic', notes: 'distinction'},
-  ];
-  shakespeareStudents = [
-    {name: 'Nevaeh Daniel', school: 'Austin East Magnet', category: 'shakespeare', notes: 'winner', best: true},
-    {name: 'Asia Chesney', school: 'Austin East Magnet', category: 'shakespeare', notes: 'distinction'},
-    {name: 'Brycen Ritchie', school: 'West High School', category: 'shakespeare', notes: 'distinction'},
-  ];
-  musicalStudents = [
-    {name: 'Katelyn Hedrick', school: 'Homeschool', category: 'musical', notes: 'winner'},
-    {name: 'Heather Bohan', school: 'Farragut', category: 'musical', notes: 'distinction'},
-    {name: 'Ella Trisler', school: 'Catholic', category: 'musical', notes: 'distinction'},
-  ];
+export class ActingComponent {
   winningTeam = [
     {name: 'Kate Bishop', category: 'Comedic'},
     {name: 'Meredith Bishop', category: 'Dramatic'},
@@ -36,9 +13,43 @@ export class ActingComponent implements OnInit {
     {name: 'Addie Peterson', category: 'Musical Theatre'},
   ];
 
-  constructor() { }
+  categories = [
+    {
+      category: 'Comedic Monologue',
+      entrants: [
+        { name: 'Grace Ballard', school: 'Maryville High School', category: 'comedic', notes: 'winner' },
+        { name: 'Kate Bishop', school: 'Grace Christian Academy', category: 'comedic', notes: 'distinction' },
+        { name: 'Kyla Jernigan', school: 'Homeschool', category: 'comedic', notes: 'distinction' }
+      ] 
+    },
+    {
+      category: 'Dramatic Monologue',
+      entrants: [
+        {name: 'LoRen Seagrave', school: 'Austin East', category: 'dramatic', notes: 'winner'},
+        {name: 'Chloe Adsit', school: 'Heritage', category: 'dramatic', notes: 'distinction'},
+        {name: 'Jackson Ahern', school: 'Austin East Magnet', category: 'dramatic', notes: 'distinction'},
+        {name: 'Heather Bohan', school: 'Farragut', category: 'dramatic', notes: 'distinction'},
+        {name: 'Jack Lloyd', school: 'Oak Ridge', category: 'dramatic', notes: 'distinction'},
+        {name: 'Ella Trisler', school: 'Catholic', category: 'dramatic', notes: 'distinction'}
+      ] 
+    },
+    {
+      category: 'Shakespeare Monologue',
+      entrants: [
+        {name: 'Nevaeh Daniel', school: 'Austin East Magnet', category: 'shakespeare', notes: 'winner', best: true},
+        {name: 'Asia Chesney', school: 'Austin East Magnet', category: 'shakespeare', notes: 'distinction'},
+        {name: 'Brycen Ritchie', school: 'West High School', category: 'shakespeare', notes: 'distinction'}
+      ] 
+    },
+    {
+      category: 'Musical Theatre',
+      entrants: [
+        {name: 'Katelyn Hedrick', school: 'Homeschool', category: 'musical', notes: 'winner'},
+        {name: 'Heather Bohan', school: 'Farragut', category: 'musical', notes: 'distinction'},
+        {name: 'Ella Trisler', school: 'Catholic', category: 'musical', notes: 'distinction'}
+      ] 
+    },
+    
 
-  ngOnInit() {
-  }
-
+  ]
 }
