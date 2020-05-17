bf4e4a25653f97db24e16f3c6aa45e9f974c1a4c
Dennis <dkmullen@gmail.com>
2020-05-17 12:00:11 -0400

    update front page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -18,7 +18,55 @@ export class HomeComponent implements OnInit {
   cards = [
     {
       title: 'NEW: ACE Interviews',
-      subtitle: 'Interview: Actor Courtney Lucien',
+      subtitle: 'Actor Kim Martin-Cotten',
+      image: `assets/images/kim-martin-cotten.jpg`,
+      alt: 'Actor Kim Martin-Cotten',
+      photocredit: 'Photo by Lois Greenfield',
+      message1: 'An extensive theatre career',
+      message2: 'From Louisville to LA to Chicago to Broadway',
+      button1: {
+        buttontxt: 'Watch',
+        link: '/features/videos',
+        fragment: 'kim-martin-cotten',
+        navMethod: 'internal',
+        disabled: false,
+        display: true
+      },
+      button2: {
+        buttontxt: null,
+        link: null,
+        navMethod: null,
+        disabled: true,
+        display: false
+      }
+    }, 
+    {
+      title: 'NEW: ACE Interviews',
+      subtitle: 'Actor Matt Lytle',
+      image: `assets/images/m_lytle.jpg`,
+      alt: 'Actor Matt Lytle',
+      photocredit: '',
+      message1: 'The art and business of acting',
+      message2: 'An interview with a versatile actor',
+      button1: {
+        buttontxt: 'Watch',
+        link: '/features/videos',
+        fragment: 'matt-lytle',
+        navMethod: 'internal',
+        disabled: false,
+        display: true
+      },
+      button2: {
+        buttontxt: null,
+        link: null,
+        navMethod: null,
+        disabled: true,
+        display: false
+      }
+    }, 
+    {
+      title: 'NEW: ACE Interviews',
+      subtitle: 'Actor Courtney Lucien',
       image: `assets/images/c_lucien.jpg`,
       alt: 'Actor Courtney Lucien',
       photocredit: 'Popio-Stumpf Photography',
@@ -42,7 +90,7 @@ export class HomeComponent implements OnInit {
     }, 
     {
       title: 'NEW: ACE Interviews',
-      subtitle: 'Interview: Author Alan Gratz',
+      subtitle: 'Author Alan Gratz',
       image: `assets/images/a_gratz.jpg`,
       alt: 'Author Alan Gratz',
       photocredit: 'Alan Gratz photo by Wes Stitt',
@@ -71,7 +119,7 @@ export class HomeComponent implements OnInit {
       alt: 'Four actors',
       photocredit: '',
       message1: 'From high school to Broadway',
-      message2: 'Four actors speak about the journey',
+      message2: 'Four actors speak about getting started',
       button1: {
         buttontxt: 'Watch',
         link: '/features/videos',
@@ -88,102 +136,102 @@ export class HomeComponent implements OnInit {
         display: false
       }
     },
-    {
-      title: 'Safer@Home Dance Contest',
-      subtitle: 'Congratulations to our winner!',
-      image: `assets/images/safe@home2020/dance.jpg`,
-      alt: 'Young man dancing',
-      photocredit: 'Photo by Marc A. Sporys on Unsplash',
-      message1: 'Students thru Grade 12',
-      message2: 'Prizes: $150, $100',
-      button1: {
-        buttontxt: 'Results',
-        link: '/events/dance',
-        navMethod: 'internal',
-        disabled: false,
-        display: true
-      },
-      button2: {
-        buttontxt: null,
-        link: null,
-        navMethod: null,
-        disabled: true,
-        display: false
-      }
-    },
-    {
-      title: 'Safer@Home Writing Contest',
-      subtitle: 'Thanks to all our writers!',
-      image: `assets/images/safe@home2020/write.jpg`,
-      alt: 'Several women writing',
-      photocredit: 'Photo by Alexis Brown on Unsplash',
-      message1: 'Students thru Grade 12',
-      message2: 'Prizes: $150, $100',
-      button1: {
-        buttontxt: 'Results',
-        link: '/events/writing',
-        navMethod: 'internal',
-        disabled: false,
-        display: true
-      },
-      button2: {
-        buttontxt: null,
-        link: null,
-        navMethod: null,
-        disabled: true,
-        display: false
-      }
-    },
-    {
-      title: 'Safer@Home Shakespeare Contest',
-      subtitle: 'Entries due April 23, 2020',
-      image: `assets/images/safe@home2020/shakespeare.jpg`,
-      alt: 'Shakespeare chalk drawing',
-      photocredit: 'Photo by Jessica Pamp on Unsplash',
-      message1: 'Students thru Grade 12',
-      message2: '2 Prizes: $100 ea.',
-      button1: {
-        buttontxt: 'Results',
-        link: '/events/shakespeare',
-        navMethod: 'internal',
-        disabled: false,
-        display: true
-      },
-      button2: {
-        buttontxt: null,
-        link: null,
-        navMethod: null,
-        disabled: true,
-        display: false
-      }
-    },
-    {
-      title: 'Safer@Home Photography Contest',
-      subtitle: 'Voting has ended',
-      image: `assets/images/safe@home2020/photography.jpg`,
-      alt: 'Woman photographing cherry blossoms',
-      photocredit: 'Photo by kazuend on Unsplash',
-      message1: 'Students thru Grade 12',
-      message2: 'Prizes: $150, $100',
-      button1: {
-        buttontxt: 'Results',
-        link: '/events/photography',
-        navMethod: 'internal',
-        disabled: false,
-        display: true
-      },
-      button2: {
-        buttontxt: null,
-        link: null,
-        navMethod: null,
-        disabled: true,
-        display: false
-      }
-    },    
+    // {
+    //   title: 'Safer@Home Dance Contest',
+    //   subtitle: 'Congratulations to our winner!',
+    //   image: `assets/images/safe@home2020/dance.jpg`,
+    //   alt: 'Young man dancing',
+    //   photocredit: 'Photo by Marc A. Sporys on Unsplash',
+    //   message1: 'Students thru Grade 12',
+    //   message2: 'Prizes: $150, $100',
+    //   button1: {
+    //     buttontxt: 'Results',
+    //     link: '/events/dance',
+    //     navMethod: 'internal',
+    //     disabled: false,
+    //     display: true
+    //   },
+    //   button2: {
+    //     buttontxt: null,
+    //     link: null,
+    //     navMethod: null,
+    //     disabled: true,
+    //     display: false
+    //   }
+    // },
+    // {
+    //   title: 'Safer@Home Writing Contest',
+    //   subtitle: 'Thanks to all our writers!',
+    //   image: `assets/images/safe@home2020/write.jpg`,
+    //   alt: 'Several women writing',
+    //   photocredit: 'Photo by Alexis Brown on Unsplash',
+    //   message1: 'Students thru Grade 12',
+    //   message2: 'Prizes: $150, $100',
+    //   button1: {
+    //     buttontxt: 'Results',
+    //     link: '/events/writing',
+    //     navMethod: 'internal',
+    //     disabled: false,
+    //     display: true
+    //   },
+    //   button2: {
+    //     buttontxt: null,
+    //     link: null,
+    //     navMethod: null,
+    //     disabled: true,
+    //     display: false
+    //   }
+    // },
+    // {
+    //   title: 'Safer@Home Shakespeare Contest',
+    //   subtitle: 'Entries due April 23, 2020',
+    //   image: `assets/images/safe@home2020/shakespeare.jpg`,
+    //   alt: 'Shakespeare chalk drawing',
+    //   photocredit: 'Photo by Jessica Pamp on Unsplash',
+    //   message1: 'Students thru Grade 12',
+    //   message2: '2 Prizes: $100 ea.',
+    //   button1: {
+    //     buttontxt: 'Results',
+    //     link: '/events/shakespeare',
+    //     navMethod: 'internal',
+    //     disabled: false,
+    //     display: true
+    //   },
+    //   button2: {
+    //     buttontxt: null,
+    //     link: null,
+    //     navMethod: null,
+    //     disabled: true,
+    //     display: false
+    //   }
+    // },
+    // {
+    //   title: 'Safer@Home Photography Contest',
+    //   subtitle: 'Voting has ended',
+    //   image: `assets/images/safe@home2020/photography.jpg`,
+    //   alt: 'Woman photographing cherry blossoms',
+    //   photocredit: 'Photo by kazuend on Unsplash',
+    //   message1: 'Students thru Grade 12',
+    //   message2: 'Prizes: $150, $100',
+    //   button1: {
+    //     buttontxt: 'Results',
+    //     link: '/events/photography',
+    //     navMethod: 'internal',
+    //     disabled: false,
+    //     display: true
+    //   },
+    //   button2: {
+    //     buttontxt: null,
+    //     link: null,
+    //     navMethod: null,
+    //     disabled: true,
+    //     display: false
+    //   }
+    // },    
     {
       title: 'Interview: Actor Kate Arrington',
       subtitle: 'of the Steppenwolf Theatre Company',
-      image: `assets/images/jay_kate.jpg`,
+      image: `assets/images/kate_arrington.jpg`,
       alt: '',
       // photocredit: 'Photo by Jessica Pamp on Unsplash',
       message1: 'The perspective and experience',
@@ -204,95 +252,95 @@ export class HomeComponent implements OnInit {
         display: false
       }
     },
-    {
-      title: 'Safer@Home Instrumental Contest',
-      subtitle: 'Thanks to all our participants',
-      image: `assets/images/safe@home2020/instrument.jpg`,
-      alt: 'Girl playing a keyboard',
-      photocredit: 'Photo by Fitsum Admasu on Unsplash',
-      message1: 'Students thru Grade 12',
-      message2: 'Prizes: $150, $100',
-      button1: {
-        buttontxt: 'Results',
-        link: 'https://www.aceknox.com/events/instrumental',
-        navMethod: 'internalWithPageLoad',
-        disabled: false,
-        display: true
-      },
-      button2: {
-        buttontxt: null,
-        link: null,
-        navMethod: null,
-        disabled: true,
-        display: false
-      }
-    },
-    {
-      title: 'Ace Acting Awards',
-      subtitle: 'January 26, 2020',
-      image: `assets/images/acting_2020/${this.actingWinners[this.getRandomInt(4)]}`,
-      alt: 'Young actor',
-      message1: 'University of Tennessee',
-      message2: 'Thanks to all for participating!',
-      button1: {
-        buttontxt: 'Results',
-        link: '/events/acting',
-        navMethod: 'internal',
-        disabled: false,
-        display: true
-      },
-      button2: {
-        buttontxt: null,
-        link: null,
-        navMethod: null,
-        disabled: true,
-        display: false
-      }
-    },
-    {
-      title: 'Ace Singing Awards',
-      subtitle: 'January 11, 2020 - 7pm',
-      image: `assets/images/singing_2020/${this.singingWinners[this.getRandomInt(5)]}`,
-      alt: 'Photo of a musical artist',
-      message1: 'Bijou Theatre',
-      message2: 'Thanks for a great show!',
-      button1: {
-        buttontxt: 'Results',
-        link: 'events/singing',
-        navMethod: 'internal',
-        disabled: false,
-        display: true
-      },
-      button2: {
-        buttontxt: null,
-        link: null,
-        navMethod: null,
-        disabled: true,
-        display: false
-      }
-    },
-    {
-      title: 'Singer-Songwriter Contest',
-      subtitle: 'January 2020',
-      image: 'assets/images/etc/songwriters2.jpg',
-      alt: 'Photo collage of young woman with a guitar',
-      message1: 'Online contest',
-      message2: 'Featuring some excellent local student-artists',
-      button1: {
-        buttontxt: 'Results',
-        link: 'events/singer-songwriter',
-        navMethod: 'internal',
-        disabled: false,
-        display: true
-      },
-      button2: {
-        buttontxt: null,
-        link: null,
-        navMethod: 'internalWithPageLoad',
-        disabled: true,
-        display: false
-      }
-    },
+    // {
+    //   title: 'Safer@Home Instrumental Contest',
+    //   subtitle: 'Thanks to all our participants',
+    //   image: `assets/images/safe@home2020/instrument.jpg`,
+    //   alt: 'Girl playing a keyboard',
+    //   photocredit: 'Photo by Fitsum Admasu on Unsplash',
+    //   message1: 'Students thru Grade 12',
+    //   message2: 'Prizes: $150, $100',
+    //   button1: {
+    //     buttontxt: 'Results',
+    //     link: 'https://www.aceknox.com/events/instrumental',
+    //     navMethod: 'internalWithPageLoad',
+    //     disabled: false,
+    //     display: true
+    //   },
+    //   button2: {
+    //     buttontxt: null,
+    //     link: null,
+    //     navMethod: null,
+    //     disabled: true,
+    //     display: false
+    //   }
+    // },
+    // {
+    //   title: 'Ace Acting Awards',
+    //   subtitle: 'January 26, 2020',
+    //   image: `assets/images/acting_2020/${this.actingWinners[this.getRandomInt(4)]}`,
+    //   alt: 'Young actor',
+    //   message1: 'University of Tennessee',
+    //   message2: 'Thanks to all for participating!',
+    //   button1: {
+    //     buttontxt: 'Results',
+    //     link: '/events/acting',
+    //     navMethod: 'internal',
+    //     disabled: false,
+    //     display: true
+    //   },
+    //   button2: {
+    //     buttontxt: null,
+    //     link: null,
+    //     navMethod: null,
+    //     disabled: true,
+    //     display: false
+    //   }
+    // },
+    // {
+    //   title: 'Ace Singing Awards',
+    //   subtitle: 'January 11, 2020 - 7pm',
+    //   image: `assets/images/singing_2020/${this.singingWinners[this.getRandomInt(5)]}`,
+    //   alt: 'Photo of a musical artist',
+    //   message1: 'Bijou Theatre',
+    //   message2: 'Thanks for a great show!',
+    //   button1: {
+    //     buttontxt: 'Results',
+    //     link: 'events/singing',
+    //     navMethod: 'internal',
+    //     disabled: false,
+    //     display: true
+    //   },
+    //   button2: {
+    //     buttontxt: null,
+    //     link: null,
+    //     navMethod: null,
+    //     disabled: true,
+    //     display: false
+    //   }
+    // },
+    // {
+    //   title: 'Singer-Songwriter Contest',
+    //   subtitle: 'January 2020',
+    //   image: 'assets/images/etc/songwriters2.jpg',
+    //   alt: 'Photo collage of young woman with a guitar',
+    //   message1: 'Online contest',
+    //   message2: 'Featuring some excellent local student-artists',
+    //   button1: {
+    //     buttontxt: 'Results',
+    //     link: 'events/singer-songwriter',
+    //     navMethod: 'internal',
+    //     disabled: false,
+    //     display: true
+    //   },
+    //   button2: {
+    //     buttontxt: null,
+    //     link: null,
+    //     navMethod: 'internalWithPageLoad',
+    //     disabled: true,
+    //     display: false
+    //   }
+    // },
     // {
     //   title: 'Battle of the Bands',
     //   subtitle: 'May 2020',
